dd346e6d07cb631eac505e173abb3a12d81ea218
Garret Gallant <gallanttechnological@gmail.com>
2020-09-01 04:19:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,6 +85,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
+    programmer_hash[:yukihiro_matsumoto => {
+      :known_for => "LISP",
+      :languages => "C"
+    }]
+    
 
 end
 
